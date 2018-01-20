4c50f5f5b0b657e81b1d4db1d712ecc1463b04df
Scott Bower <scottbower@zaphod.local>
2018-01-20 08:34:43 -0500

    draft

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,21 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+
+if (process.env.JAWSDB_URL) {
+connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
   port: 3306,
   host: "localhost",
   user: "root",
   password: "",
   database: "burgers_db"
-});
+  });
+};
+
+
 
 // Make connection.
 connection.connect(function(err) {
